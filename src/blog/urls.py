9a33126e113c29aa79432cb23706d69f490eb047
juliozapatao96 <julio.cesar.zaor@gmail.com>
2025-05-10 17:14:25 -0500

    feat: add pagination in post list

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -6,7 +6,8 @@ app_name = 'blog'
 
 urlpatterns = [
     # post views
-    path('', views.post_list, name='post_list'),
+    #path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'), # Cambiado a la vista basada en clase PostListView
     #El segundo patrón se asigna a la vista post_detail y solo acepta un argumento, id, que coincide con un entero, definido por el convertidor de rutas int.
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', 
          views.post_detail, 
