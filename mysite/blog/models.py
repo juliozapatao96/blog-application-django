7174d5af80e8b26a40aa5bd6447193be6a1ad0b2
juliozapatao96 <julio.cesar.zaor@gmail.com>
2025-05-10 10:37:42 -0500

    implements get_absolute_url in models.py

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -16,7 +17,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish') # SlugField: campo de texto que se utiliza para crear URLs legibles
     author = models.ForeignKey(User, # ForeignKey: relación de uno a muchos
                                on_delete=models.CASCADE,# on_delete=models.CASCADE: si se elimina el usuario, se eliminan los posts
                                related_name='blog_posts') # related_name: nombre del campo inverso
@@ -40,4 +41,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        # reverse: genera la URL de la vista 'post_detail'
+        # args: se pasan como una lista de argumentos a la URL (en este caso, el id del post)
+        return reverse('blog:post_detail', args=[self.id])
+        
     
